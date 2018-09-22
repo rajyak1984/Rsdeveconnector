7cbc345d0242b83209de5ee7355863c6618587c8
rajyak1984 <rashmikolluru@gmail.com>
2018-09-22 11:57:49 +0100

    Added JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://raji:Rs1984@ds163182.mlab.com:63182/rsdevconnector'
-}
+    mongoURI: 'mongodb://raji:Rs1984@ds163182.mlab.com:63182/rsdevconnector',
+    secretOrKey: 'secret'
+};
