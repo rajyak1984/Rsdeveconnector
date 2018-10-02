21d19dc7b92d1e1a1916244c174e423f4433df4a
rajyak1984 <rashmikolluru@gmail.com>
2018-10-02 18:20:21 +0100

    Added React components and Frontend

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,73 @@
 import React, { Component } from 'react'
 
  class Register extends Component {
+   constructor(){
+       super();
+       this.state = {
+           name: '',
+           email:'',
+           password:'',
+           password:'',
+           errors: {}
+       }
+   }
+
   render() {
     return (
-      <div>
-        <h1>Register</h1>
+      
+        <div className="register">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 m-auto">
+          <h1 className="display-4 text-center">Sign Up</h1>
+          <p className="lead text-center">Create your TechConnector account</p>
+          <form action="create-profile.html">
+            <div className="form-group">
+              <input 
+              type="text" 
+              className="form-control form-control-lg" 
+              placeholder="Name" 
+              name="name" 
+              value ={this.state.name}
+              />
+            </div>
+            <div className="form-group">
+              <input 
+              type="email" 
+              className="form-control form-control-lg" 
+              placeholder="Email Address" 
+              name="email" 
+              value={this.state.email}
+              />
+              <small classNameName="form-text text-muted"> Use a Gravatar email or Gravatar image. </small>
+            </div>
+            <div className="form-group">
+              <input 
+              type="password" 
+              className="form-control form-control-lg" 
+              placeholder="Password" 
+              name="password" 
+              value={this.state.password}
+              />
+            </div>
+            <div className="form-group">
+              <input 
+              type="password" 
+              className="form-control form-control-lg" 
+              placeholder="Confirm Password" 
+              name="password2" 
+              value={this.state.password2}
+              />
+            </div>
+            <input 
+            type="submit" 
+            className="btn btn-info btn-block mt-4" />
+          </form>
+        </div>
       </div>
+    </div>
+  </div>
+      
     )
   }
 }
