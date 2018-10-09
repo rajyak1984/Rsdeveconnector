dfb1e5100c1a086d34a041a4926364d267b0f25f
rajyak1984 <rashmikolluru@gmail.com>
2018-10-09 14:49:35 +0100

    Added more react stuff to integrate backend

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react'
-import axios from 'axios';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { registerUser} from '../../actions/authActions';
  class Register extends Component {
    constructor(){
        super();
@@ -15,6 +18,12 @@ import classnames from 'classnames';
        this.onSubmit = this.onSubmit.bind(this);
    }
 
+   componentWillRecieveProps(nextProps) {
+     if(nextProps.errors) {
+       this.setState({errors: nextProps.errors});
+     }
+   }
+
    onChange(e){
      this.setState({[e.target.name]:e.target.value});  
    }
@@ -26,16 +35,18 @@ import classnames from 'classnames';
            password:this.state.password,
            password2:this.state.password2
        }
-
-       axios.post('/api/users/register', newUser)
-       .then(res => console.log())
-       .catch(err => this.setState({errors: err.response.data}))
-   }
+       
+       this.props.registerUser(newUser, this.props.history);
+      }
+  //      
+  //  
   render() {
       const {errors} = this.state;
+
     return (
       
         <div className="register">
+        {/* {user ? user.name: null} */}
     <div className="container">
       <div className="row">
         <div className="col-md-8 m-auto">
@@ -111,4 +122,15 @@ import classnames from 'classnames';
   }
 }
 
-export default Register;
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+ 
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  errors: state.errors
+})
+
+export default connect(mapStateToProps, { registerUser }) (withRouter(Register));
